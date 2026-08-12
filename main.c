0c47e0cbbe86bc079e449d0f34a36d850279ff9d
wangwu <123@qq.co>
2026-08-12 14:00:18 +0800

    守护进程

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include "App_Application.h"
 #include "App_OTA.h"
+#include "App_Daemon.h"
 int main( int num, char* args[] ){
     if(num <= 1){
         log_info("必须传入app/ota/deamon参数");
@@ -10,8 +11,8 @@ int main( int num, char* args[] ){
         App_Application_Run();
     }else if( strcmp(args[1] , "ota") == 0 ){
         App_OTA_Run();
-    }else if( strcmp(args[1], "deamon") == 0 ){
-
+    }else if( strcmp(args[1], "daemon") == 0 ){
+        App_Daemon_Run();
     }else{
         log_info("参数传入错误");
     }
